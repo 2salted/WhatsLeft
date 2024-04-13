dbb936711905a9264444dbcf5c5d2cdf621f9f42
2salted <noamgraf123@gmail.com>
2024-04-13 14:46:53 +0000

    added user signup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,23 @@
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 import { Stack } from 'expo-router/stack';
+import { SafeAreaView } from 'react-native';
+const clerkApiKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+import React from "react";
+import SignUpScreen from '@/components/SignUpScreen';
 
 export default function AppLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <ClerkProvider publishableKey={clerkApiKey!}>
+      <SafeAreaView>
+        <SignedIn>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </SignedIn>
+        <SignedOut>
+          <SignUpScreen />
+        </SignedOut>
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
