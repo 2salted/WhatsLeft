c9d7cb443f6796851851e3e04a708408fbcb497e
2salted <noamgraf123@gmail.com>
2024-04-13 17:01:21 +0000

    added token cache

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,28 @@ const clerkApiKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 import React from "react";
 import SignUpScreen from '@/components/SignUpScreen';
 import SignInScreen from '@/components/SignInScreen';
+import * as SecureStore from "expo-secure-store";
 
 export default function AppLayout() {
+
+  const tokenCache = {
+    async getToken(key: string) {
+      try {
+        return SecureStore.getItemAsync(key);
+      } catch (err) {
+        return null;
+      }
+    },
+    async saveToken(key: string, value: string) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (err) {
+        return;
+      }
+    },
+  };
   return (
-    <ClerkProvider publishableKey={clerkApiKey!}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={clerkApiKey!}>
       <SignedIn>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
