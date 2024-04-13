f674520fe720ded2d20f1164ac7d291e1f5456df
2salted <noamgraf123@gmail.com>
2024-04-13 00:25:51 +0000

    added filedbase routing and css styles

---
+++ app/(tabs)/calls.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function Tab() {
+  return (
+    <View style={{ backgroundColor: 'black', justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
+export default function Tab() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, backgroundColor: 'black' }}>
+      <Text>Messages</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
