6532998ac8bcf1d8f8aff9972d09bc6373cc58b0
2salted <noamgraf123@gmail.com>
2024-04-14 23:47:22 +0000

    working post using node for mongoDB

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from '@clerk/clerk-expo';
+import { Stack } from 'expo-router';
 import { View, Text, SafeAreaView } from 'react-native';
 
 export default function Tab() {
@@ -9,8 +10,14 @@ export default function Tab() {
 
   return (
     <SafeAreaView className='flex-1 bg-black'>
+      <Stack.Screen
+        options={{
+          headerTitle: "",
+          headerShadowVisible: false,
+        }}
+      />
       <View>
-        <Text className='text-gray-50'>Chats</Text>
+        <Text className='text-gray-50 text-4xl font-bold px-3'>Chats</Text>
       </View>
     </SafeAreaView>
   );
--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,46 @@
 import express from 'express'
-import 'dotenv/config'
+import dotenv from "dotenv";
+dotenv.config();
 import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = process.env.MONGODB_CONNECTION
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+
+async function main() {
+  const uri = process.env.MONGODB_CONNECTION
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  const app = express()
+  const port = 3000
+  await createListing(client,
+    {
+      name: "Lovely Loft",
+      summary: "A charming loft in Paris",
+      bedrooms: 1,
+      bathrooms: 1
+    }
+  );
+  async function createListing(client, newListing) {
+    const result = await client.db("whatsleft").collection("users").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
   }
-});
+  app.listen(port, async () => {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+    console.log(`Example app listening on port ${port}`)
+  })
+}
 
-const app = express()
-const port = 3000
+main().catch(console.error);
 
-app.get('/', (req, res) => {
-  res.send("hello")
-})
-
-app.listen(port, async () => {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-  console.log(`Example app listening on port ${port}`)
-})
