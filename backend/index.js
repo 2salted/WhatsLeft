a18f890f67dc3ad5454f94204b08163b50c73cd5
2salted <noamgraf123@gmail.com>
2024-04-14 23:55:43 +0000

    working backend post to mongoDB using NodeJS

--- backend/index.js
+++ backend/index.js
@@ -15,18 +15,6 @@ async function main() {
   });
   const app = express()
   const port = 3000
-  await createListing(client,
-    {
-      name: "Lovely Loft",
-      summary: "A charming loft in Paris",
-      bedrooms: 1,
-      bathrooms: 1
-    }
-  );
-  async function createListing(client, newListing) {
-    const result = await client.db("whatsleft").collection("users").insertOne(newListing);
-    console.log(`New listing created with the following id: ${result.insertedId}`);
-  }
   app.listen(port, async () => {
     try {
       // Connect the client to the server	(optional starting in v4.7)
@@ -40,6 +28,15 @@ async function main() {
     }
     console.log(`Example app listening on port ${port}`)
   })
+  await createUser(client,
+    {
+      user: "salted",
+    }
+  );
+  async function createUser(client, newUser) {
+    const result = await client.db("whatsleft").collection("users").insertOne(newUser);
+    console.log(`New user created with the following id: ${result.insertedId}`);
+  }
 }
 
 main().catch(console.error);
