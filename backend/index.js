0d7372fdebba444c28b19b4e40901972c3741471
2salted <noamgraf123@gmail.com>
2024-04-16 00:24:26 +0000

    update

--- backend/index.js
+++ backend/index.js
@@ -3,41 +3,34 @@ import dotenv from "dotenv";
 dotenv.config();
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-async function main() {
-  const uri = process.env.MONGODB_CONNECTION
+const uri = process.env.MONGODB_CONNECTION
 
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  const app = express()
-  const port = 3000
-  app.listen(port, async () => {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-    console.log(`Example app listening on port ${port}`)
-  })
-  await createUser(client,
-    {
-      user: "JOEBIDEN",
-    }
-  );
-  async function createUser(client, newUser) {
-    const result = await client.db("whatsleft").collection("users").insertOne(newUser);
-    console.log(`New user created with the following id: ${result.insertedId}`);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
+});
+const app = express()
+app.use(express.json())
+const port = 3000
+app.post('/user', async (req, res) => {
+  console.log(req)
+  await createUser(client, { name: req.body.name, clerkId: req.body.clerkId })
+  res.send({ success: true })
+})
+async function createUser(client, newUser) {
+  const result = await client.db("whatsleft").collection("users").insertOne(newUser);
+  console.log(`New user created with the following id: ${result.insertedId}`);
 }
-
-main().catch(console.error);
+app.listen(port, async () => {
+  try {
+    await client.connect();
+    await client.db("whatsleft").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+  console.log(`Example app listening on port ${port}`)
+})
 
