f17dc30687eb350c6854ceb6ce6690246d5f85d2
2salted <noamgraf123@gmail.com>
2024-04-17 20:29:15 +0000

    stylized add user modal, working user fetching to display users

--- app/addUserModal.tsx
+++ app/addUserModal.tsx
@@ -1,24 +1,70 @@
-import { SafeAreaView, TextInput, View } from 'react-native';
-import { Link, router } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { ActivityIndicator, Pressable, SafeAreaView, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+import { useEffect, useState } from 'react';
+
+type User = {
+  firstName: string,
+  lastName: string,
+  clerkId: string,
+}
+
 export default function addUserModal() {
+  const [searchUser, setSearchUser] = useState<string>("");
+  const [usersData, setUsersData] = useState<User[] | undefined>(undefined);
+  const [showSpinner, setShowSpinner] = useState<boolean>(false)
+
+  async function fetchUsers() {
+    try {
+      setShowSpinner(true)
+      const response = await fetch('http://192.168.0.148:3000/search');
+      if (!response.ok) {
+        throw new Error('Failed to fetch users');
+      }
+      const data = await response.json();
+      return data
+    } catch (error) {
+      console.error('Error fetching users');
+      return []
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then(users => {
+        setUsersData(users)
+        setShowSpinner(false)
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+  }, [searchUser])
 
   return (
     <SafeAreaView className='flex-1 bg-[#1e1e1e]'>
       <ExpoStatusBar style='light' hidden={false} />
       <View className='flex-col'>
-        <View className='px-4'>
+        <View className='px-4 pb-3'>
           <TextInput
             className="bg-zinc-700 px-2 py-2 text-base rounded-xl
-          leading-5 text-white"
+            leading-5 text-white"
             placeholder="Search users by name"
+            value={searchUser}
             placeholderTextColor="#8e8e8e"
+            onChangeText={(searchUser) => setSearchUser(searchUser)}
           />
         </View>
-        <View>
-
-        </View>
+        <ScrollView className='px-4'>
+          {showSpinner ? <ActivityIndicator size='large' />
+            : usersData?.map((user, index) => {
+              return (
+                <View key={index} className="py-3">
+                  <TouchableOpacity className='p-3 bg-[#2e2e2e] rounded-xl flex-row'>
+                    <Text className='text-gray-50 text-base font-bold'>{user.firstName}</Text>
+                  </TouchableOpacity>
+                </View>
+              )
+            })}
+        </ScrollView>
       </View>
     </SafeAreaView>
   )
