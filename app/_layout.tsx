377ccd73afe96a63e7e1ad3ea42cac4f4226db6d
2salted <noamgraf123@gmail.com>
2024-05-02 11:05:37 +0000

    fixed ANDROID problem with async storage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,22 +14,24 @@ import { Link } from "expo-router";
 
 export default function AppLayout(): React.JSX.Element {
   const [test, setTest] = React.useState<boolean>(false);
+
   const tokenCache = {
     async getToken(key: string) {
       try {
-        return SecureStore.getItemAsync(key);
+        return await SecureStore.getItemAsync(key);
       } catch (err) {
         return null;
       }
     },
     async saveToken(key: string, value: string) {
       try {
-        return SecureStore.setItemAsync(key, value);
+        return await SecureStore.setItemAsync(key, value);
       } catch (err) {
         return;
       }
     },
   };
+
   return (
     <ClerkProvider tokenCache={tokenCache} publishableKey={clerkApiKey!}>
       <ExpoStatusBar hidden={false} translucent={false} style="light" />
