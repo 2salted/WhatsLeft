366818f190a4ae9ec714b13a1439759fe8ba9ebb
2salted <noamgraf123@gmail.com>
2024-05-05 19:11:37 +0000

    added styles to entire messaging page

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,9 +1,10 @@
-import { FlatList, Image, KeyboardAvoidingView, Platform, Pressable, SafeAreaView, Text, TextInput, TouchableOpacity, View, useWindowDimensions } from "react-native";
+import { FlatList, Image, KeyboardAvoidingView, Platform, SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { Stack } from "expo-router/stack";
 import { useEffect, useState } from "react";
 import { useAuth } from "@clerk/clerk-expo";
 import { useLocalSearchParams } from "expo-router";
 import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
 
 type User = {
   firstName: string;
@@ -19,6 +20,7 @@ export default function Messaging(): React.JSX.Element {
   const [image, setImage] = useState<string>("")
   const [receiverUserInfo, setreceiverUserInfo] = useState<User>()
   const [userMessage, setUserMessage] = useState<string>();
+  const [messages, setMessages] = useState<any>();
 
   async function get15Messages(userId: string, otherUserId: any) {
     try {
@@ -58,8 +60,10 @@ export default function Messaging(): React.JSX.Element {
     get15Messages(userId ?? "", otherUserId)
   }, [])
 
+  console.log(messages)
+
   return (
-    <View className="flex-1">
+    <View className="flex-1 bg-neutral-900">
       <SafeAreaView className="flex-1 bg-black">
         <Stack.Screen
           options={{
@@ -99,20 +103,30 @@ export default function Messaging(): React.JSX.Element {
           }}
         />
         <View className="flex-1">
-          <View className="flex-1">
+        </View>
+      </SafeAreaView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={60}
+      >
+        <View className="bg-neutral-800 text-lg pb-6 pt-3 px-4 text-white">
+          <View className="pb-4 flex-row">
+            <View className="flex-1 pr-4">
+              <TextInput
+                className="bg-neutral-700 text-white p-2 rounded-full border border-neutral-600"
+                value={userMessage}
+                placeholderTextColor="#8e8e8e"
+                onChangeText={(message) => setUserMessage(message)}
+              />
+            </View>
+            <TouchableOpacity className="rounded-full justify-center bg-green-500" onPress={() => {
+              setMessages(userMessage)
+            }}>
+              <Ionicons name="send" size={20} color="black" style={{ marginRight: 6, marginLeft: 10 }} />
+            </TouchableOpacity>
           </View>
         </View>
-      </SafeAreaView >
-      <View className="bg-neutral-800 text-lg pb-8 pt-2 text-white">
-        <View className="pb-4">
-          <TextInput
-            className="bg-red-600 pb-3"
-            value={userMessage}
-            placeholderTextColor="#8e8e8e"
-            onChangeText={(message) => setUserMessage(message)}
-          />
-        </View>
-      </View>
-    </View>
+      </KeyboardAvoidingView>
+    </View >
   )
 }
